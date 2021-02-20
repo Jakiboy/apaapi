f62376171172a03999089c0291052c82c426bdd9
JIHAD SINNAOUR <j.sinnaour.official@gmail.com>
2021-02-20 13:52:52 +0100

    Update version
    Merge pull request #5 from gnadelwartz/master

--- src/lib/ItemOperation.php
+++ src/lib/ItemOperation.php
@@ -1,6 +1,7 @@
 <?php
 /**
  * @package Amazon Product Advertising API
+ * @version 1.0.7
  * @copyright (c) 2019 - 2020 Jakiboy
  * @author Jihad Sinnaour <mail@jihadsinnaour.com>
  * @link https://jakiboy.github.io/apaapi/
