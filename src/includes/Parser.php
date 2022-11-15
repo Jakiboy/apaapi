795ee21d13f577011b642e52c3e59c410bc4c790
JIHAD SINNAOUR <j.sinnaour.official@gmail.com>
2022-11-15 15:25:56 +0100

    Update version

--- src/includes/Parser.php
+++ src/includes/Parser.php
@@ -2,7 +2,7 @@
 /**
  * @author    : JIHAD SINNAOUR
  * @package   : Apaapi | Amazon Product Advertising API Library (v5)
- * @version   : 1.1.5
+ * @version   : 1.1.6
  * @copyright : (c) 2019 - 2022 Jihad Sinnaour <mail@jihadsinnaour.com>
  * @link      : https://jakiboy.github.io/apaapi/
  * @license   : MIT
