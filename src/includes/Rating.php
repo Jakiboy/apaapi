23b8ba5040a02022bf8c15dc67af1c187d638c48
Jakiboy <j.sinnaour.official@gmail.com>
2025-01-29 15:24:37 +0100

    Update Rating.php

--- src/includes/Rating.php
+++ src/includes/Rating.php
@@ -15,10 +15,26 @@ declare(strict_types=1);
 namespace Apaapi\includes;
 
 /**
- * @deprecated Use Scrapper Class instead
+ * Apaapi rating scraper.
  */
-final class Rating extends Scrapper
+final class Rating extends Scraper
 {
+    public const SELECTORS = [
+        'value' => "//div[contains(@class,'AverageCustomerReviews')]",
+        'count' => "//div[contains(@class,'averageStarRatingNumerical')]"
+    ];
+    public const PATTERN   = [
+        'count' => '/\D/'
+    ];
+
+    /**
+     * @inheritdoc
+     */
+    public function __construct(string $keyword, string $locale = 'com', ?string $tag = null)
+    {
+        parent::__construct($keyword, $locale, $tag);
+    }
+
     /**
      * Get item rating data.
      *
@@ -27,6 +43,26 @@ final class Rating extends Scrapper
      */
     public function get() : array
     {
-        return $this->getRating();
+        return $this->parse();
+    }
+
+    /**
+     * @inheritdoc
+     */
+    protected function format(array $data) : array
+    {
+        // Format rating value
+        $element = $data['value'] ?? '';
+        $element = explode(' ', $element);
+        $element = $element[0] ?? '';
+        $data['value'] = Normalizer::toFloat($element);
+
+        // Format rating count
+        $pattern = self::PATTERN['count'];
+        $element = $data['count'] ?? '';
+        $element = Normalizer::removeRegex($pattern, $element);
+        $data['count'] = Normalizer::toInt($element);
+
+        return $data;
     }
 }
