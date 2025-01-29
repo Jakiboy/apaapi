c30db4a2c4f5f7de26bd1dfac0795eedd1a3fbc1
Jakiboy <j.sinnaour.official@gmail.com>
2025-01-29 15:24:59 +0100

    Create ScraperException.php

---
+++ src/exceptions/ScraperException.php
@@ -0,0 +1,25 @@
+<?php
+/**
+ * @author    : Jakiboy
+ * @package   : Amazon Product Advertising API Library (v5)
+ * @version   : 1.3.x
+ * @copyright : (c) 2019 - 2025 Jihad Sinnaour <mail@jihadsinnaour.com>
+ * @link      : https://jakiboy.github.io/apaapi/
+ * @license   : MIT
+ *
+ * This file if a part of Apaapi Lib.
+ */
+
+namespace Apaapi\exceptions;
+
+/**
+ * Apaapi scraper exception.
+ */
+final class ScraperException extends \Exception
+{
+    public static function invalidKeyword(?string $item = null) : string
+    {
+        $item = $item ?: 'undefined';
+        return "Invalid ASIN or ISBN keyword '{$item}'";
+    }
+}
